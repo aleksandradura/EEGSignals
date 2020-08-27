0fb91e78712c62255fcf3568e7daa5c949873f92
aleksandradura <aleksandradura.ad@gmail.com>
2020-08-27 19:20:01 +0200

    add fvalue to algorithm

--- ANOVA.py
+++ ANOVA.py
@@ -20,13 +20,13 @@ df = pd.read_csv('C:\\Users\\aleks\\OneDrive\\Pulpit\\data_preprocessed_python\\
                  #     index_col=False)
 # x = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 # x = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32']
-x = ['1t', '2t', '3t', '4t', '5t', '6t', '7t', '8t', '9t', '10t', '11t', '12t', '13t', '14t', '15t', '16t', '17t', '18t', '19t', '20t', '21t', '22t', '23t', '24t', '25t', '26t', '27t', '28t', '29t', '30t', '31t', '32t', '1a', '2a', '3a', '4a', '5a', '6a', '7a', '8a', '9a', '10a', '11a', '12a', '13a', '14a', '15a', '16a', '17a', '18a', '19a', '20a', '21a', '22a', '23a', '24a', '25a', '26a', '27a', '28a', '29a', '30a', '31a', '32a', '1b', '2b', '3b', '4b', '5b', '6b', '7b', '8b', '9b', '10b', '11b', '12b', '13b', '14b', '15b', '16b', '17b', '18b', '19b', '20b', '21b', '22b', '23b', '24b', '25b', '26b', '27b', '28b', '29b', '30b', '31b', '32b'
-]
+x = ['1t', '2t', '3t', '4t', '5t', '6t', '7t', '8t', '9t', '10t', '11t', '12t', '13t', '14t', '15t', '16t', '17t', '18t', '19t', '20t', '21t', '22t', '23t', '24t', '25t', '26t', '27t', '28t', '29t', '30t', '31t', '32t', '1a', '2a', '3a', '4a', '5a', '6a', '7a', '8a', '9a', '10a', '11a', '12a', '13a', '14a', '15a', '16a', '17a', '18a', '19a', '20a', '21a', '22a', '23a', '24a', '25a', '26a', '27a', '28a', '29a', '30a', '31a', '32a', '1b', '2b', '3b', '4b', '5b', '6b', '7b', '8b', '9b', '10b', '11b', '12b', '13b', '14b', '15b', '16b', '17b', '18b', '19b', '20b', '21b', '22b', '23b', '24b', '25b', '26b', '27b', '28b', '29b', '30b', '31b', '32b',
+'1g', '2g', '3g', '4g', '5g', '6g', '7g', '8g', '9g', '10g', '11g', '12g', '13g', '14g', '15g', '16g', '17g', '18g', '19g', '20g', '21g', '22g', '23g', '24g', '25g', '26g', '27g', '28g', '29g', '30g', '31g']
 # print(df)
 # df.boxplot(column= ['1t', '2t', '3t', '4t', '5t', '6t', '7t', '8t', '9t', '10t', '11t', '12t', '13t', '14t', '15t', '16t', '17t', '18t', '19t', '20t', '21t', '22t', '23t', '24t', '25t', '26t', '27t', '28t', '29t', '30t', '31t', '32t', '1a', '2a', '3a', '4a', '5a', '6a', '7a', '8a', '9a', '10a', '11a', '12a', '13a', '14a', '15a', '16a', '17a', '18a', '19a', '20a', '21a', '22a', '23a', '24a', '25a', '26a', '27a', '28a', '29a', '30a', '31a', '32a', '1b', '2b', '3b', '4b', '5b', '6b', '7b', '8b', '9b', '10b', '11b', '12b', '13b', '14b', '15b', '16b', '17b', '18b', '19b', '20b', '21b', '22b', '23b', '24b', '25b', '26b', '27b', '28b', '29b', '30b', '31b', '32b'
 # ], grid=False)
 # plt.show()
-allChannels = 96
+allChannels = 127
 
 # fvalue, pvalue = stats.f_oneway(df[x[0]], df[x[1]])
 def takeFirstChannel():
@@ -38,7 +38,7 @@ def takeFirstChannel():
             if numpy.isnan(pvalue):
                 pass
             else:
-                summ += pvalue
+                summ += float(pvalue)
                 ammount += 1
                 # print(str(i) + '  -  ' + str(j) + '  -  ' + str(pvalue))
         result.append(summ/(ammount))
@@ -54,15 +54,23 @@ def takeFirstChannel():
     return channel
 # print(takeFirstChannel())
 def takeSecondChannel():
-
-    smallest, secCh = 0.0, 0
+    fList, chList = [], []
+    smallest, secCh, small, chan = 0.0, 0, 0, 0
     ch = takeFirstChannel()
     if ch - 1 != 0:
-        fvalue, pvalue = stats.f_oneway(df[x[ch - 1]], df[x[0]])
+        fvalue, pvalue = stats.f_oneway(abs(df[x[ch - 1]]), abs(df[x[0]]))
         secCh = 1
+        if pvalue == 0.0:
+            fList.append(fvalue)
+            chList.insert(0, 0)
+            print(fvalue)
     else:
-        fvalue, pvalue = stats.f_oneway(df[x[ch - 1]], df[x[1]])
+        fvalue, pvalue = stats.f_oneway(abs(df[x[ch - 1]]), abs(df[x[1]]))
         secCh = 2
+        if pvalue == 0.0:
+            fList.append(fvalue)
+            chList.insert(0, 1)
+            print(fvalue)
     if numpy.isnan(pvalue):
         # print("Null")
         pass
@@ -71,7 +79,7 @@ def takeSecondChannel():
     # print(smallest)
     for j in range(allChannels):
         if ch - 1 != j:
-            fvalue, pvalue = stats.f_oneway(df[x[ch-1]], df[x[j]])
+            fvalue, pvalue = stats.f_oneway(abs(df[x[ch-1]]), abs(df[x[j]]))
             # print(str(j) + '  -  ' + str(pvalue))
             if numpy.isnan(pvalue):
                 pass
@@ -79,11 +87,22 @@ def takeSecondChannel():
                 if pvalue < smallest:
                     smallest = pvalue
                     secCh = j + 1
-    # print(smallest)
-    # print(secCh)
-    return secCh
+                if pvalue == 0.0:
+                    fList.append(fvalue)
+                    print(str(j+1) + '  -  ' + str(fvalue))
+                    chList.append(j)
+    if len(fList) != 0:
+        small = fList[0]
+        for i in range(1, len(fList)):
+            if fList[i] > small:
+                small = fList[i]
+                chan = chList[i] + 1
+        return chan
+    else:
+        return secCh
 # print(takeSecondChannel())
 ch = takeFirstChannel()
+
 secCh = takeSecondChannel()
 result = []
 summary = []
@@ -91,8 +110,8 @@ summ, channel = 0, 1
 thirdCh = 0
 for j in range(allChannels):
     if j != ch - 1 and j != secCh - 1:
-        fstat1 = stats.f_oneway(df[x[ch - 1]], df[x[j]])
-        fstat2 = stats.f_oneway(df[x[secCh - 1]], df[x[j]])
+        fstat1 = stats.f_oneway(abs(df[x[ch - 1]]), abs(df[x[j]]))
+        fstat2 = stats.f_oneway(abs(df[x[secCh - 1]]), abs(df[x[j]]))
         summ = fstat1.pvalue + fstat2.pvalue
         # print(summ/2)
         summary.append(summ/2)
@@ -101,10 +120,7 @@ for j in range(allChannels):
     elif j == secCh - 1:
         summary.insert(j, 1)
     summ = 0
-    # if pvalue < result[i]:
-    #     result.append(pvalue)
-    #     channel = i + 1
-    # print(fvalue, pvalue)
+
 res = summary[0]
 for i in range(len(summary)):
     if summary[i] < res:
@@ -120,9 +136,9 @@ print(thirdCh)
 s = []
 for j in range(allChannels):
     if j != ch - 1 and j != secCh - 1 and j != thirdCh:
-        fstat1 = stats.f_oneway(df[x[ch - 1]], df[x[j]])
-        fstat2 = stats.f_oneway(df[x[secCh - 1]], df[x[j]])
-        fstat3 = stats.f_oneway(df[x[thirdCh - 1]], df[x[j]])
+        fstat1 = stats.f_oneway(abs(df[x[ch - 1]]), abs(df[x[j]]))
+        fstat2 = stats.f_oneway(abs(df[x[secCh - 1]]), abs(df[x[j]]))
+        fstat3 = stats.f_oneway(abs(df[x[thirdCh - 1]]), abs(df[x[j]]))
         summ = fstat1.pvalue + fstat2.pvalue + fstat3.pvalue
         # print(summ/2)
         s.append(summ/3)
@@ -137,12 +153,12 @@ for j in range(allChannels):
     #     result.append(pvalue)
     #     channel = i + 1
     # print(fvalue, pvalue)
-res = s[0]
-for i in range(len(s)):
-    if s[i] < res:
-        res = s[i]
-        fourthCh = i + 1
-print(fourthCh)
+# res = s[0]
+# for i in range(len(s)):
+#     if s[i] < res:
+#         res = s[i]
+#         fourthCh = i + 1
+# print(fourthCh)
 # for i in range(3):
 #     for j in range(1, 3):
 #         fvalue, pvalue = stats.f_oneway(df[x[i]], df[x[j]])
@@ -154,3 +170,12 @@ print(fourthCh)
 # print(fvalue, pvalue)
 # fvalue, pvalue = stats.f_oneway(df['1'], df['3'])
 # print(fvalue, pvalue)
+# from sympy.mpmath import *
+# import mpmath as mpf
+
+# for i in range(127):
+#
+#
+#     fvalue, pvalue = stats.f_oneway(abs(df[x[68]]), abs(df[x[i]]))
+#     if pvalue == 0.0:
+#         print(str(pvalue) + '  -  ' + str(fvalue))
