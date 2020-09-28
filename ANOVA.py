537751666e5bbd9e6d4a7f139b14dfb4a861e68f
aleksandradura <aleksandradura.ad@gmail.com>
2020-09-28 19:26:18 +0200

    spaghetti code | time for refactoring

--- ANOVA.py
+++ ANOVA.py
@@ -16,13 +16,13 @@ import numpy
 df = pd.read_csv('C:\\Users\\aleks\\OneDrive\\Pulpit\\data_preprocessed_python\\asdf.csv', sep=' ',
                  # columns = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32'],
                      index_col=False)
-# x = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32']
-x = ['1t', '2t', '3t', '4t', '5t', '6t', '7t', '8t', '9t', '10t', '11t', '12t', '13t', '14t', '15t', '16t', '17t', '18t', '19t', '20t', '21t', '22t', '23t', '24t', '25t', '26t', '27t', '28t', '29t', '30t', '31t', '32t', '1a', '2a', '3a', '4a', '5a', '6a', '7a', '8a', '9a', '10a', '11a', '12a', '13a', '14a', '15a', '16a', '17a', '18a', '19a', '20a', '21a', '22a', '23a', '24a', '25a', '26a', '27a', '28a', '29a', '30a', '31a', '32a', '1b', '2b', '3b', '4b', '5b', '6b', '7b', '8b', '9b', '10b', '11b', '12b', '13b', '14b', '15b', '16b', '17b', '18b', '19b', '20b', '21b', '22b', '23b', '24b', '25b', '26b', '27b', '28b', '29b', '30b', '31b', '32b', '1g', '2g', '3g', '4g', '5g', '6g', '7g', '8g', '9g', '10g', '11g', '12g', '13g', '14g', '15g', '16g', '17g', '18g', '19g', '20g', '21g', '22g', '23g', '24g', '25g', '26g', '27g', '28g', '29g', '30g', '31g', '32g']
+x = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32']
+# x = ['1t', '2t', '3t', '4t', '5t', '6t', '7t', '8t', '9t', '10t', '11t', '12t', '13t', '14t', '15t', '16t', '17t', '18t', '19t', '20t', '21t', '22t', '23t', '24t', '25t', '26t', '27t', '28t', '29t', '30t', '31t', '32t', '1a', '2a', '3a', '4a', '5a', '6a', '7a', '8a', '9a', '10a', '11a', '12a', '13a', '14a', '15a', '16a', '17a', '18a', '19a', '20a', '21a', '22a', '23a', '24a', '25a', '26a', '27a', '28a', '29a', '30a', '31a', '32a', '1b', '2b', '3b', '4b', '5b', '6b', '7b', '8b', '9b', '10b', '11b', '12b', '13b', '14b', '15b', '16b', '17b', '18b', '19b', '20b', '21b', '22b', '23b', '24b', '25b', '26b', '27b', '28b', '29b', '30b', '31b', '32b', '1g', '2g', '3g', '4g', '5g', '6g', '7g', '8g', '9g', '10g', '11g', '12g', '13g', '14g', '15g', '16g', '17g', '18g', '19g', '20g', '21g', '22g', '23g', '24g', '25g', '26g', '27g', '28g', '29g', '30g', '31g', '32g']
 # print(df)
 # df.boxplot(column= ['1t', '2t', '3t', '4t', '5t', '6t', '7t', '8t', '9t', '10t', '11t', '12t', '13t', '14t', '15t', '16t', '17t', '18t', '19t', '20t', '21t', '22t', '23t', '24t', '25t', '26t', '27t', '28t', '29t', '30t', '31t', '32t', '1a', '2a', '3a', '4a', '5a', '6a', '7a', '8a', '9a', '10a', '11a', '12a', '13a', '14a', '15a', '16a', '17a', '18a', '19a', '20a', '21a', '22a', '23a', '24a', '25a', '26a', '27a', '28a', '29a', '30a', '31a', '32a', '1b', '2b', '3b', '4b', '5b', '6b', '7b', '8b', '9b', '10b', '11b', '12b', '13b', '14b', '15b', '16b', '17b', '18b', '19b', '20b', '21b', '22b', '23b', '24b', '25b', '26b', '27b', '28b', '29b', '30b', '31b', '32b'
 # ], grid=False)
 # plt.show()
-allChannels = 128
+allChannels = 32
 
 # fvalue, pvalue = stats.f_oneway(df[x[0]], df[x[1]])
 def takeFirstChannel():
@@ -112,14 +112,19 @@ def thirdChanel():
             fvalue2, pvalue2 = stats.f_oneway(abs(df[x[secCh - 1]]), abs(df[x[j]]))
             summ = pvalue + pvalue2
             summary.append(summ/2)
+            print(summ/2)
             if (summ/2) == 0.0:
                 fList.append((fvalue + fvalue2)/2)
                 chList.append(j + 1)
                 # print(str(j + 1) + '  -  ' + str((fvalue + fvalue2)/2))
+            else:
+                chList.append(j + 1)
         elif j == ch - 1:
             summary.insert(j, 1)
+            chList.append(j + 1)
         elif j == secCh - 1:
             summary.insert(j, 1)
+            chList.append(j + 1)
         summ = 0
     if len(fList) != 0:
         small = fList[0]
@@ -129,7 +134,12 @@ def thirdChanel():
                 chan = chList[i]
         return chan
     else:
-        return "Nie ma"
+        small = summary[0]
+        for k in range(len(summary)):
+            if small > summary[k]:
+                small = summary[k]
+                ch = chList[k]
+        return ch
 
 
 # print(ch)
@@ -161,6 +171,8 @@ def fourthChanel():
                 fList.append((fvalue + fvalue2 + fvalue3)/3)
                 chList.append(m + 1)
                 # print(str(m + 1) + '  -  ' + str((fvalue + fvalue2 + fvalue3)/3))
+            else:
+                chList.append(m + 1)
         elif m == ch - 1:
             su.insert(m, 1)
         elif m == secCh - 1:
@@ -176,8 +188,12 @@ def fourthChanel():
                 chan = chList[i]
         return chan
     else:
-        return "Nie ma"
-        # print(m)
+        small = su[0]
+        for k in range(len(su)):
+            if small > su[k]:
+                small = su[k]
+                ch = chList[k]
+        return ch
 
         # print(su[m])
     # if len(fList) != 0:
